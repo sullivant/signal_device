79ed6c3c06171c78b4c0600930110ffdd95bc592
Thomas Sullivan <thomas.sullivan@protedyne.com>
2021-03-09 13:27:45 -0500

    now parsing the yaml config file in ../resources

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,13 @@ pub struct SignalDevice {
     client: tcp::Transport,
     resource_location: String, // the location of the configuration file for this device
 
-    // TODO: Turn these into an array of signals structs
-    signal_num: u16,
-    signal_status: bool,
+    signals: Vec<Signal>,
 }
 
 pub struct Signal {
-    signal_num: u16,
     signal_name: String,
+    signal_type: String,
+    signal_offset: u16,
     signal_status: bool,
 }
 
@@ -31,23 +30,62 @@ impl Signal {
             false => "OFF".to_string(),
         }
     }
+
+    pub fn new(signal_name: String, signal_type: String, signal_offset: u16) -> Signal {
+        println!(
+            "Creating signal: {}, type: {}, offset: {}",
+            signal_name, signal_type, signal_offset
+        );
+
+        Signal {
+            signal_name,
+            signal_type,
+            signal_offset,
+            signal_status: false,
+        }
+    }
+
+    pub fn get_signal_name(&self) -> &String {
+        &self.signal_name
+    }
+
+    pub fn get_signal_offset(&self) -> &u16 {
+        &self.signal_offset
+    }
+    pub fn get_signal_status(&self) -> &bool {
+        //TODO: Make this updated via a scanner thread instead of via direct calls through
+        //SignalDevice
+        &self.signal_status
+    }
 }
 
 impl SignalDevice {
-    pub fn get_signal(&mut self) -> bool {
-        //let mut client = tcp::Transport::new(&self.device).unwrap();
-        let val: bool = get_signal_status(&mut self.client, self.signal_num);
+    // Search through the hash of all signals, find the one by name referenced, and return
+    // a reference to the struct <Signal>
+    pub fn get_signal(&mut self) -> &Signal {
+        //TODO: Make this return a named signal or ERR
+        &self.signals.first().unwrap()
+    }
 
-        val
+    pub fn get_signal_directly(&mut self) -> bool {
+        //TODO Make this use a named signal or return ERR
+        let signal: &Signal = self.get_signal();
+        let signal_offset: u16 = signal.get_signal_offset().clone();
+        get_signal_status(&mut self.client, signal_offset)
     }
 
     pub fn get_device(&mut self) -> &String {
         &self.device
     }
+
+    pub fn get_resource_location(&mut self) -> &String {
+        &self.resource_location
+    }
 }
 
 pub fn new(device_name: String) -> SignalDevice {
     let resource_location: String = format!("./resources/{}.yaml", device_name);
+    println!("Creating device: {}", device_name);
     println!("Using device configuration at: {}", resource_location);
     let file = File::open(resource_location.clone()).expect("Unable to open file.");
     let mut buf_reader = BufReader::new(file);
@@ -63,22 +101,36 @@ pub fn new(device_name: String) -> SignalDevice {
     let coupler_raw = &device_conf["device"]["coupler"];
     let coupler = coupler_raw.as_str().unwrap_or("127.0.0.1");
 
-    // Get signals information
-    let signal_offset_raw = &device_conf["signals"]["diInputSensor"]["offset"];
-    let signal_offset: u16 = signal_offset_raw.as_i64().unwrap_or(0) as u16;
+    // Get signals information out of the config file and create signal objects
+    // for each record in the signals: hash
+    let mut signals: Vec<Signal> = Vec::new();
 
-    println!(
-        "Creating device at: {} with signal offset of: {}",
-        coupler, signal_offset
-    );
+    for signal_vec in device_conf["signals"].clone().as_vec() {
+        for signal in signal_vec {
+            let signal_name: String = signal["name"]
+                .clone()
+                .into_string()
+                .unwrap_or(format!("invalid_signal_name"));
+
+            let signal_type: String = signal["type"]
+                .clone()
+                .into_string()
+                .unwrap_or(format!("invalid_signal_type"));
+
+            let signal_offset: u16 = signal["offset"].clone().as_i64().unwrap_or(0) as u16;
+
+            let sig: Signal = Signal::new(signal_name, signal_type, signal_offset);
+            signals.push(sig);
+        }
+    }
 
     let client = tcp::Transport::new(&coupler).unwrap();
+
     SignalDevice {
         device: coupler.to_string(),
         client: client,
         resource_location: resource_location,
-        signal_num: signal_offset,
-        signal_status: false,
+        signals: signals,
     }
 }
 
